30298d21b530cf7ede02e42e5f5ebf8423587ec5
Abhishek Charpe <acharpe@affinionds.com>
2019-10-13 00:40:21 +0530

    added select image for upload option

--- src/app/seller-post-ad-form/seller-post-ad-form.component.ts
+++ src/app/seller-post-ad-form/seller-post-ad-form.component.ts
@@ -5,5 +5,36 @@ import { Component} from '@angular/core';
   templateUrl: './seller-post-ad-form.component.html',
   styleUrls: ['./seller-post-ad-form.component.css']
 })
-export class SellerPostAdFormComponent  {
+
+export class SellerPostAdFormComponent  {  
+
+  imageUrl = ["","","","",""];
+  imageValue = ["none","none","none","none","none"];
+  CrossBtnValue = ["none","none","none","none","none"];
+  buttonName = ["Add","Add","Add","Add","Add"];
+  iconOfButton = ["plus" ,"plus","plus","plus","plus"]; 
+
+  addImage(id,event){
+    console.log(id+"  "+event.target.value);
+    this.imageUrl[id] = event.target.value;
+    this.CrossBtnValue[id] ="";
+    this.imageValue[id] ="";
+    this.buttonName[id] ="Edit";
+    this.iconOfButton[id] ="edit";
+  }
+
+  removeImage(id)
+  {
+    this.iconOfButton[id] = "plus";
+    this.imageValue[id] = "none";
+    this.CrossBtnValue[id] = "none";
+    this.buttonName[id] = "Add";
+    this.iconOfButton[id] = "plus";
+  }
+
+  selectHeroImg(id)
+  {
+    console.log(id+" is hero image");
+  }
+
 }

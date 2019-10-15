38d6d8f5e2bb8736e02d57030fc4daa52ed29306
Rai <rarai@internal.tavisca.com>
2019-10-15 13:20:53 +0530

    structuring added as per angular standards

---
+++ src/app/shared-component/shared-test-component/shared-test-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shared-test-component',
+  templateUrl: './shared-test-component.component.html',
+  styleUrls: ['./shared-test-component.component.css']
+})
+export class SharedTestComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

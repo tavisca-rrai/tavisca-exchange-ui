9abf4abc5a0dc6f13026dfa5934bdc91e25e66ad
Narkhede <nnarkhede@internal.tavisca.com>
2019-10-23 19:58:32 +0530

    need to remove script links from index.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { SharedTestComponentComponent } from './shared-component/shared-test-component/shared-test-component.component';
 import { AdvertismentsListComponent } from './home-module/advertisments-list/advertisments-list.component';
 import { AdvertismentDetailsComponent } from './home-module/advertisment-details/advertisment-details.component';
+import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SharedTestComponentComponent,
     AdvertismentsListComponent,
-    AdvertismentDetailsComponent
+    AdvertismentDetailsComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule

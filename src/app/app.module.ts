91c4040b7b780d351dd016661f873a4e5dc4b126
tavisca-cpadole <51901832+tavisca-cpadole@users.noreply.github.com>
2019-10-24 09:57:32 +0530

    Made changes acccording to the comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { SharedTestComponentComponent } from './shared-component/shared-test-component/shared-test-component.component';
-import { AdvertismentsListComponent } from './home-module/advertisments-list/advertisments-list/advertisments-list.component';
+import { AppComponent } from "./app.component";
+import { SharedTestComponentComponent } from "./shared-component/shared-test-component/shared-test-component.component";
+import { AdvertismentsListComponent } from "./home-module/advertisments-list/advertisments-list.component";
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SharedTestComponentComponent,
-    AdvertismentsListComponent
+    AdvertismentsListComponent,
+    LoginComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

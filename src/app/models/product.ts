d3b0d9726573afb3afd0a58b6bcae899964ade9c
Abhishek Charpe <acharpe@affinionds.com>
2019-10-24 17:09:29 +0530

    changes in objects

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -2,12 +2,10 @@ import { Price } from './price';
 import { Address } from './address';
 
 export class Product {
- 
     public title: string;
     public category: string;
     public description: string;
     public dateOfPurchase: Date;
-    public isNegotiable: boolean;
     public heroImageUrl: string;
     public imageUrl: string[];
     public price : Price;

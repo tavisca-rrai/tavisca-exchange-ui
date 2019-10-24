07446cc455a5e93d19c358166e44e143a6708149
tavisca-vnagaraju <vnagaraju@tavisca.in>
2019-10-24 19:51:52 +0530

    Added login and home page wiring

--- src/app/login-module/login-component/login.component.ts
+++ src/app/login-module/login-component/login.component.ts
@@ -1,14 +1,14 @@
 import { LoginService } from "../../services/login-services/login.service";
 import { Component, OnInit } from "@angular/core";
 import { UserSignInDetails } from "../models/user-signin-details";
-
+import { Router } from "@angular/router";
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService,private router: Router) { }
   userDetails: UserSignInDetails;
   isInvalid = false;
   ngOnInit() {
@@ -17,9 +17,7 @@ export class LoginComponent implements OnInit {
   tryLogin() {
     this.loginService.verifyUserCredentials(this.userDetails).subscribe(
       response => {
-        //temporary, after completeion we will redirect to home page
-        console.log(response);
-        alert("You are successfully logged in!");
+        this.router.navigateByUrl("/products");
       },
       err => {
         console.log(err.error);

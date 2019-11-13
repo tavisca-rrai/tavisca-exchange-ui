f6f08ec4fc9efaddfb860036e1c9548f463ef4ca
tavisca-rpandey <rpandey@tavisca.com>
2019-11-13 18:20:32 +0530

    Minor changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -21,12 +21,11 @@ export const environment = {
     inactiveAds: "/inactive",
     userId: "777888666",
   },
-  },
+  
   imageApiSettings:{
     BaseUrl: "https://localhost:44357/",
     uploadImagePath: "api/v1.0/OnlineRetailPortal/image/",
     storeImagePath: "api/v1.0/OnlineRetailPortal/image/store",
-
     mockImageUrl: "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
   }
 };

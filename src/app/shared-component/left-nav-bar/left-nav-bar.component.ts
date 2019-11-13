421519505134a4599f45861e07dbd006cd74c018
tavisca-vnagaraju <vnagaraju@tavisca.in>
2019-11-13 17:04:17 +0530

    added categories , changed mobile view for side bar

--- src/app/shared-component/left-nav-bar/left-nav-bar.component.ts
+++ src/app/shared-component/left-nav-bar/left-nav-bar.component.ts
@@ -15,8 +15,7 @@ export class LeftNavBarComponent implements OnInit {
   selectedSort:string = "DateDSC";
   productSortOptions:ProductSort;
   sortOptions:SortOptions;
-  content = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42];
-  categories = ["Electronics","Clothing","Books","Sports"];
+  categories = ["Property","Car","Furniture","Mobile","Bike","Book","Fashion","Electronic","Other"];
   options: Options = {
     floor: 0,
     ceil: 50000,
